843c0f3f723a89c23911f02b583e241965cffd07
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-18 23:08:37 +0700

    Tugas 8 JS

---
+++ Tugas 8 JS.js
@@ -0,0 +1,13 @@
+function objek() {
+    var orang = {
+        Nama: "Fikri Sandi",
+        Umur: "19 Tahun",
+        Asal: "Gresik",
+        Gender: "Laki-Laki"
+    }
+    console.log(orang);
+    orang.Asal = "Bandung";
+    console.log(orang);
+}
+
+objek();
